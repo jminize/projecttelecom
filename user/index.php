f19ec101a8932f42738e4524616ed02b5c4220df
jminize <nuaninjakkrit@gmail.com>
2020-02-23 14:58:52 +0700

    edit user index.php

--- user/index.php
+++ user/index.php
@@ -200,32 +200,6 @@ while($row=$sql->fetch_assoc()){
       <!--====================================================footer====================================================-->
     </div>
   </div>
-  
-  <!--modal location detail-->
-<div class="modal fade" id="modal_location" tabindex="1" role="dialog" aria-labelledby="Modalterminal" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">สถานที่นั่ง</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div id="row">
-          <div class="col-12">
-          รายละเอียดโซนที่นั่ง : 
-            <span id="show_location"></span>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-<!--modal location detail-->
 
  <!--======================================= Modal logout ===============================================-->
     
